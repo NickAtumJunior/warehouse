80c0eda738b7956ffdc3ca7dec7a6b828afe0152
NickAtumJunior <nithishthirucelvam007@gmail.com>
2022-06-15 13:54:06 +0530

    signin commit

---
+++ src/Pages/Signup.js
@@ -0,0 +1,14 @@
+import { Grid, Stack } from '@mui/material'
+import React from 'react'
+
+function Signup() {
+  return (
+    <Grid container xs={12} md={8} alignItems='center'>
+      <Stack spacing={2}>
+           
+      </Stack>
+    </Grid>
+  )
+}
+
+export default Signup
